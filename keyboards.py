06c4bda7e3558b629e5628c17280fafd2efa2b89
qobz1e <131813746+qobz1e@users.noreply.github.com>
2024-05-10 00:25:39 +0300

    добавлен топ команд
    Изменены кнопки в главном меню в связи с добавлением топа команд

--- keyboards.py
+++ keyboards.py
@@ -3,15 +3,15 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Изменить отслеживаемые команды"))
+    keyboard.add(KeyboardButton("Топ команд"))
+    keyboard.add(KeyboardButton("Отслеживаемые команды"))
     keyboard.add(KeyboardButton("Матчи"))
-    keyboard.add(KeyboardButton("Турниры"))
     return keyboard
 
 
 def send_matches_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Активные матчи"))
+    keyboard.add(KeyboardButton("Live матчи"))
     keyboard.add(KeyboardButton("Список интересующих матчей"))
     keyboard.add(KeyboardButton("Список всех будущих матчей"))
     keyboard.add(KeyboardButton("⬅️ В главное меню"))
