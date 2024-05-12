412752fe92abaf3194bf73ec05c07f8e206e86e1
qobz1e <131813746+qobz1e@users.noreply.github.com>
2024-05-12 15:52:45 +0300

    changed language

--- keyboards.py
+++ keyboards.py
@@ -3,41 +3,41 @@ from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 
 def main_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Топ команд"))
-    keyboard.add(KeyboardButton("Отслеживаемые команды"))
-    keyboard.add(KeyboardButton("Матчи"))
+    keyboard.add(KeyboardButton("Ranking"))
+    keyboard.add(KeyboardButton("Monitored teams"))
+    keyboard.add(KeyboardButton("Matches"))
     return keyboard
 
 
 def send_matches_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Live матчи"))
-    keyboard.add(KeyboardButton("Список интересующих матчей"))
-    keyboard.add(KeyboardButton("Список всех будущих матчей"))
-    keyboard.add(KeyboardButton("⬅️ В главное меню"))
+    keyboard.add(KeyboardButton("Live matches"))
+    keyboard.add(KeyboardButton("Matches of monitored teams"))
+    keyboard.add(KeyboardButton("All upcoming matches"))
+    keyboard.add(KeyboardButton("⬅️ Main menu"))
     return keyboard
 
 
 def send_events_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("Активные турниры"))
-    keyboard.add(KeyboardButton("Будущие турниры"))
-    keyboard.add(KeyboardButton("⬅️ В главное меню"))
+    keyboard.add(KeyboardButton("Live events"))
+    keyboard.add(KeyboardButton("Upcoming events"))
+    keyboard.add(KeyboardButton("⬅️ Main menu"))
     return keyboard
 
 
 def if_caption_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     keyboard.row(
-        KeyboardButton("✅ Да"),
-        KeyboardButton("❌ Нет")
+        KeyboardButton("✅ Yes"),
+        KeyboardButton("❌ No")
     )
-    keyboard.add(KeyboardButton("⬅️ В главное меню"))
+    keyboard.add(KeyboardButton("⬅️ Main menu"))
 
     return keyboard
 
 
 def back_to_menu_keyboard():
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(KeyboardButton("⬅️ В главное меню"))
+    keyboard.add(KeyboardButton("⬅️ Main menu"))
     return keyboard
